15a034f1669bcac48167a60714d94aaf58eef94a
kadijaconteh <kadijac634@gmail.com>
2026-02-18 17:50:36 +0100

    Create easyexercises.py
    First practice exercises

---
+++ easyexercises.py
@@ -0,0 +1,6 @@
+# Excercise 1 - Write a program that:
+# Asks the user for their name
+# Prints: "Hello, <name>!"  
+
+name = input("Enter you name: ")   
+print(f"Hello, {name}")                           
